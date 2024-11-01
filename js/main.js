5a91eaaf66e19a8a1a56e965ef352e551a38ea12
AstVentFel <alex-sandr1092@mail.ru>
2024-11-01 16:49:29 +0300

    feat: add card for task_two

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ import { img, paragraph } from "./scenario.js";
 const wrapper = document.querySelector(".wrapper");
 
 //Смена картинок и диалогов
-let counterClickBtn = 0;
-let counterImg = 0;
+let counterClickBtn = 28;
+let counterImg = 13;
+let counterHeart = 100;
 
+const slider = document.querySelector(".slider");
 const btnHistory = document.querySelector(".slider__btn-history");
 const sliderNext = document.querySelector(".slider__next");
 const sliderText = document.querySelector(".slider__text");
+const sliderAct = document.querySelector(".slider__act");
+const popup = document.querySelector(".popup");
 
 const changeSliderText = (counterClickBtn) => {
   sliderText.textContent = paragraph[counterClickBtn];
@@ -29,13 +33,63 @@ const handleSliderChange = () => {
   changeSliderText(counterClickBtn);
 };
 
+const popupTitle = document.querySelector(".popup__title");
+const popupText = document.querySelector(".popup__text");
+
 sliderNext.addEventListener("click", () => {
+  if (counterClickBtn === 25) {
+    popup.style.backgroundImage = "url(../assets/img/act_2/die.jpg)";
+    popupTitle.textContent = "Плохая концовка";
+    popupText.textContent = "Вы умерли!";
+    popup.classList.add("popup-active");
+  }
   handleSliderChange();
   showInputDoor();
   showTextJornal();
   showprogress();
+  changeNameBtn();
+  console.log(counterClickBtn);
+  console.log(counterImg);
 });
 //
+
+const cardBox = document.querySelector(".card-box ");
+
+const changeNameBtn = () => {
+  if (counterClickBtn === 8) {
+    sliderNext.textContent = "Осмотреть дом";
+  } else if (counterClickBtn === 9) {
+    sliderNext.textContent = "продолжить";
+  } else if (counterClickBtn === 23) {
+    sliderAct.textContent = "Принять таблетки";
+    sliderNext.textContent = "Спуститься вниз";
+  } else if (counterClickBtn === 24) {
+    counterHeart -= 5;
+    showHeart(counterHeart);
+    sliderNext.textContent = "Ничего не делать";
+  } else if (counterClickBtn === 25) {
+    counterHeart -= 50;
+    showHeart(counterHeart);
+    sliderNext.textContent = "Ничего не делать";
+  } else if (counterClickBtn === 28) {
+    slider.classList.add("slider-inactive");
+    cardBox.classList.add("card-box-active");
+    task__two();
+  }
+};
+
+sliderAct.addEventListener("click", () => {
+  if (counterClickBtn >= 23) {
+    counterClickBtn = 25;
+    counterImg = 12;
+    handleSliderChange();
+    showprogress();
+    sliderNext.textContent = "Отбиться";
+    sliderAct.classList.add("slider-Act-inactive");
+  } else if (counterClickBtn <= 22) {
+    popup.classList.add("popup-active");
+  }
+});
 
 //первый квест
 const inputBox = document.querySelector(".input-box");
@@ -49,9 +103,10 @@ const showInputDoor = () => {
   }
 };
 
+//
 const startTaskOne = () => {
   if (inputDoor.value === "vriend") {
-    handleSliderChange("next");
+    handleSliderChange();
     sliderNext.textContent = "Войти внутрь";
     inputBox.classList.remove("input-box-active");
   } else if (inputDoor.value.length !== 0) {
@@ -94,3 +149,48 @@ btnHistory.addEventListener("click", () => {
 });
 
 //
+
+const boxIindicatorsHeart = document.querySelector(".box-indicators__heart");
+
+const showHeart = (counterHeart) => {
+  boxIindicatorsHeart.textContent = `Количество жизней:${counterHeart}%`;
+};
+
+cardBox.addEventListener("click", () => {});
+
+const randomNums = () => {
+  let num = 0;
+  let flag = true;
+  while (flag) {
+    num = (Math.random() * 10).toFixed();
+    if (num < 9) {
+      flag = false;
+    }
+  }
+  return num;
+};
+
+let timeCardBox = [];
+const getCardBox = () => {
+  let counterTime = 0;
+  let intervall = 500;
+  let counterHit = 20;
+  for (let index = 0; index < counterHit; index++) {
+    let randomNum = randomNums();
+    timeCardBox.push([counterTime, counterTime + intervall, randomNum]);
+    counterTime += intervall;
+  }
+};
+getCardBox();
+console.log(timeCardBox);
+
+const task__two = () => {
+  timeCardBox.forEach(([a, b, c]) => {
+    setTimeout(() => {
+      cardBox.children[c].classList.add("card-box__card-active");
+    }, a);
+    setTimeout(() => {
+      cardBox.children[c].classList.remove("card-box__card-active");
+    }, b);
+  });
+};
